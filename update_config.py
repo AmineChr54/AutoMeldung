b297d3e681d10eb0c822abc864781a617c3f5360
Amine Cheikhrouhou <amine.cheikhrouhou54@gmail.com>
2025-12-01 00:45:20 +0100

    trial version

---
+++ update_config.py
@@ -0,0 +1,7 @@
+CURRENT_VERSION = "0.9.3"
+# IMPORTANT: This URL must point to a static web server where files can be accessed individually.
+# We are using GitHub Pages for this.
+# REPLACE 'AmineChr54' with your actual GitHub username if it's different.
+UPDATE_URL = "https://aminechr54.github.io/AutoMeldung/tuf_repo/" 
+ 
+
