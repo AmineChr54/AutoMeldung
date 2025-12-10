edb589ae933b6974e74551208bb051252927a313
Amine Cheikhrouhou <amine.cheikhrouhou54@gmail.com>
2025-12-10 15:32:34 +0100

    Official Release 1.2.2

--- update_config.py
+++ update_config.py
@@ -1,4 +1,4 @@
-CURRENT_VERSION = "1.1.0"
+CURRENT_VERSION = "1.2.2"
 # IMPORTANT: This URL must point to a static web server where files can be accessed individually.
 # We are using GitHub Pages for this.
 # REPLACE 'AmineChr54' with your actual GitHub username if it's different.
